d6d987ddd56c749ec3b3634a9b3a8e12796cb947
Samantha Bajis <Sammi.Bajis@gmail.com>
2018-02-27 18:04:17 -0500

    First Commit
    Currently still working on game. Need to add animations, particle effects, sound.

---
+++ AR Treasure Hunt/Assets/EnableDisable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EnableDisable : MonoBehaviour {
+
+    public GameObject Enable_Disable;
+
+    public void Enable()
+    {
+        Enable_Disable.SetActive(true);
+    }
+    public void Disable()
+    {
+        Enable_Disable.SetActive(false);
+    }
+		
+}
