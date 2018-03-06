2550393b6b2b332069300e195d32a4d1d291b248
Samantha Bajis <Sammi.Bajis@gmail.com>
2018-03-06 00:40:04 -0500

    Finish gameplay commit
    Basic gameplay is all done. Just need to fix a few items and add restart.

---
+++ AR Treasure Hunt/Assets/ChestScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChestScript : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () 
+    {
+        anim = GetComponent<Animator>();
+	}
+
+    public void OnPointerClick()
+    {
+        anim.SetTrigger("ChestOpen");  
+    }
+	
+}
